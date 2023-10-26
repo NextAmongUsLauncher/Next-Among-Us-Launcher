0e0860cd739c76337e0167d4782ba6cd5021c6c6
Bilibili_MengChu <2133404320@qq.com>
2023-10-26 19:39:03 +0800

    Update

--- NextAmongUsLauncher/MainWindow.xaml.cs
+++ NextAmongUsLauncher/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,72 +13,51 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Graphics;
-using Windows.UI;
-using Microsoft.UI.Composition.SystemBackdrops;
-using CommunityToolkit.WinUI.Helpers;
+using WinRT.Interop;
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using Windows.Storage;
+using System.Runtime.InteropServices;
+using Windows.System;
+using System.Security.AccessControl;
 using NextAmongUsLauncher.Pages;
-using Vanara.PInvoke;
-using WinRT.Interop;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace NextAmongUsLauncher;
 
-namespace NextAmongUsLauncher
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public IntPtr hWnd;
+    private AppWindow appWindow;
+
+    public MainWindow()
     {
-        public new static MainWindow Current { get; private set; }
-        public const string ProjectName = "Next AmongUs Launcher";
-        public MainWindow()
-        {
-            Current = this;
-            InitializeComponent();
-            SetTitle();
-            OpenPage();
-            ChangeTitleBarButtonColor();
-        }
-        
-        private void ChangeTitleBarButtonColor()
-        {
-            if (!AppWindowTitleBar.IsCustomizationSupported()) return;
-            var titleBar = AppWindow.TitleBar;
-            titleBar.ButtonBackgroundColor = Colors.Transparent;
-            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
-            switch (RootGrid.ActualTheme)
-            {
-                case ElementTheme.Light:
-                    titleBar.ButtonForegroundColor = Colors.Black;
-                    titleBar.ButtonHoverForegroundColor = Colors.Black;
-                    titleBar.ButtonHoverBackgroundColor = Color.FromArgb(0x20, 0x00, 0x00, 0x00);
-                    break;
-                case ElementTheme.Dark:
-                    titleBar.ButtonForegroundColor = Colors.White;
-                    titleBar.ButtonHoverForegroundColor = Colors.White;
-                    titleBar.ButtonHoverBackgroundColor = Color.FromArgb(0x20, 0xFF, 0xFF, 0xFF);
-                    titleBar.ButtonInactiveForegroundColor = Color.FromArgb(0xFF, 0x99, 0x99, 0x99);
-                    break;
-                case ElementTheme.Default:
-                default:
-                    break;
-            }
-        }
+        this.InitializeComponent();
 
-        private void SetTitle()
-        {
-            var titleBar = AppWindow.TitleBar;
-            titleBar.ExtendsContentIntoTitleBar = true;
-            Title = ProjectName;
-        }
+        SystemBackdrop = new DesktopAcrylicBackdrop();
 
-        private void OpenPage()
+        hWnd = WindowNative.GetWindowHandle(this);
+        WindowId id = Win32Interop.GetWindowIdFromWindow(hWnd);
+        appWindow = AppWindow.GetFromWindowId(id);
+
+        appWindow.MoveAndResize(new RectInt32(_X: 335, _Y: 165, _Width: 1250, _Height: 750));
+
+        this.ExtendsContentIntoTitleBar = true;
+        this.SetTitleBar(AppTitleBar);
+
+    }
+
+    private void GloabalNavigation_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        Type pageType = ((NavigationViewItem)args.SelectedItem).Tag switch
         {
-            Main_Frame.Content = new MainPage();
-        }
+            "Play" => typeof(Page_Play),
+            "Version" => typeof(Page_Version),
+            "About" => typeof(Page_About),
+            "Setting" => typeof(Page_Setting),
+            _ => typeof(Page_Play),
+        };
+
+        _ = contentFrame.Navigate(pageType);
     }
 }
