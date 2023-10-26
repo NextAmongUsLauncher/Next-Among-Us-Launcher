4180693ee5c527ef3def216e67bc6a3562bcd6a2
Bilibili_MengChu <2133404320@qq.com>
2023-10-26 19:50:18 +0800

    Updates and code cleanup

--- NextAmongUsLauncher/MainWindow.xaml.cs
+++ NextAmongUsLauncher/MainWindow.xaml.cs
@@ -1,26 +1,8 @@
-using Microsoft.UI.Composition.SystemBackdrops;
+using System;
+using Windows.Graphics;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using WinRT.Interop;
-using Microsoft.UI;
-using Microsoft.UI.Windowing;
-using Windows.Graphics;
-using Windows.Storage;
-using System.Runtime.InteropServices;
-using Windows.System;
-using System.Security.AccessControl;
 using NextAmongUsLauncher.Pages;
 
 namespace NextAmongUsLauncher;
@@ -28,34 +10,31 @@ namespace NextAmongUsLauncher;
 public sealed partial class MainWindow : Window
 {
     public IntPtr hWnd;
-    private AppWindow appWindow;
 
     public MainWindow()
     {
-        this.InitializeComponent();
+        Current = this;
+        InitializeComponent();
 
         SystemBackdrop = new DesktopAcrylicBackdrop();
 
-        hWnd = WindowNative.GetWindowHandle(this);
-        WindowId id = Win32Interop.GetWindowIdFromWindow(hWnd);
-        appWindow = AppWindow.GetFromWindowId(id);
+        AppWindow.MoveAndResize(new RectInt32(335, 165, 1250, 750));
 
-        appWindow.MoveAndResize(new RectInt32(_X: 335, _Y: 165, _Width: 1250, _Height: 750));
-
-        this.ExtendsContentIntoTitleBar = true;
-        this.SetTitleBar(AppTitleBar);
-
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(AppTitleBar);
     }
+
+    public new static MainWindow Current { get; private set; }
 
     private void GloabalNavigation_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
     {
-        Type pageType = ((NavigationViewItem)args.SelectedItem).Tag switch
+        var pageType = ((NavigationViewItem)args.SelectedItem).Tag switch
         {
             "Play" => typeof(Page_Play),
             "Version" => typeof(Page_Version),
             "About" => typeof(Page_About),
             "Setting" => typeof(Page_Setting),
-            _ => typeof(Page_Play),
+            _ => typeof(Page_Play)
         };
 
         _ = contentFrame.Navigate(pageType);
--- NextAmongUsLauncher/Pages/Page_Play.xaml.cs
+++ NextAmongUsLauncher/Pages/Page_Play.xaml.cs
@@ -4,13 +4,14 @@ using Microsoft.UI.Xaml.Controls;
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace NextAmongUsLauncher.Pages;
+
 /// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
+///     An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
 public sealed partial class Page_Play : Page
 {
     public Page_Play()
     {
-        this.InitializeComponent();
+        InitializeComponent();
     }
 }
